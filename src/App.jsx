26c3d7333cf79fbd24d26450aa61395471acfe69
nomanqadir-cosmoinc <nomanqadir@cosmoinc.co>
2025-04-22 15:29:22 +0500

    set layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
+ 
 
 function App() {
-  useFavicon();
 
   return (
     <Router>
       <Routes>
-        <Route element={<Layout />}>
+        <Route  >
           <Route
             path="/"
             element={
               <>
-                <div className="max-lg:hidden">{/** Desktop Component */}</div>
-                <div className="lg:hidden">{/** Mobile Component */}</div>
+                <div className="max-lg:hidden"> asdasdas</div>
+                <div className="lg:hidden">asdasdas</div>
               </>
             }
           />
---
+++ src/components/mobile/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MobileHero from './mobileHero'
+
+function EvolveStrengthHome() {
+  return (
+    <div>
+      {/* <MobileHero/> */}
+      asdasdasd
+    </div>
+  )
+}
+
+export default EvolveStrengthHome
