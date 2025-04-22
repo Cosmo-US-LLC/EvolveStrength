813bebb9280e3fd75da446d2d8a18443b8ac9766
jawad <jawadakhtar@cosmoinc.co>
2025-04-22 17:05:34 +0500

    ... 2

---
+++ src/components/Home/desktop/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MobileHero from './mobileHero'
+
+function EvolveStrengthHome() {
+  return (
+    <div>
+        asdasd
+    </div>
+  )
+}
+
+export default EvolveStrengthHome
